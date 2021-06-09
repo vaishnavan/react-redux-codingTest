29a84fb66597c510e27ecb01e1a33269f54ea4d5
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2021-06-09 21:10:25 +0530

    actions

---
+++ client/src/actions/employees.js
@@ -0,0 +1,80 @@
+import {
+    CREATE_EMPLOYEE,
+    RETRIEVE_EMPLOYEES,
+    UPDATE_EMPLOYEE,
+    DELETE_EMPLOYEE,
+    DELETE_ALL_EMPLOYEES,
+} from './types';
+
+import EmployeeService from '../services/EmployeeService';
+
+export const createEmployee = (firstName, lastName, userName) => async (dispatch) => {
+    try {
+        const res = await EmployeeService.create({ firstName, lastName, userName });
+    
+        dispatch({
+          type: CREATE_EMPLOYEE,
+          payload: res.data,
+        });
+    
+        return Promise.resolve(res.data);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+}
+
+export const retrieveEmployees = () => async (dispatch) => {
+    try {
+      const res = await EmployeeService.getAll();
+  
+      dispatch({
+        type: RETRIEVE_EMPLOYEES,
+        payload: res.data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  export const updateEmployee = (id, data) => async (dispatch) => {
+    try {
+      const res = await EmployeeService.update(id, data);
+  
+      dispatch({
+        type: UPDATE_EMPLOYEE,
+        payload: data,
+      });
+  
+      return Promise.resolve(res.data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
+  
+  export const deleteEmployee = (id) => async (dispatch) => {
+    try {
+      await EmployeeService.remove(id);
+  
+      dispatch({
+        type: DELETE_EMPLOYEE,
+        payload: { id },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  export const deleteAllEmployees = () => async (dispatch) => {
+    try {
+      const res = await EmployeeService.removeAll();
+  
+      dispatch({
+        type: DELETE_ALL_EMPLOYEES,
+        payload: res.data,
+      });
+  
+      return Promise.resolve(res.data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
